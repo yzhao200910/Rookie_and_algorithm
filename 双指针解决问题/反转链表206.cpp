fb282e6c5cab8ab016e5ecfb170894675cc9ac05
SpongeBob <128422499+yzhao200910@users.noreply.github.com>
2023-09-30 22:31:58 -0400

    Initial commit

---
+++ 双指针解决问题/反转链表206.cpp
@@ -0,0 +1,40 @@
+//
+// Created by 16537 on 2023/9/30.
+//
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+class Sultion{
+    ListNode* reverseList(ListNode* head) {
+        ListNode *prenode=nullptr;
+        ListNode * Node=head;
+        ListNode * temp;
+        while(Node){
+            temp=Node->next;
+            Node->next=prenode;
+            prenode=Node;
+            Node=temp;
+        }
+        return prenode;
+    }
+
+};
+//递归的写法
+class solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        return reverse(head,nullptr);
+    }
+    ListNode* reverse(ListNode *cur,ListNode* pre){
+        if(!cur){
+            return pre;
+        }
+        ListNode *temp=cur->next;
+        cur->next=pre;
+        return reverse(temp,cur);
+    }
+};
